bb21980213a260adca16e2a756a220dcc0cee3f8
meamalpb <321amalpb.com@gmail.com>
2023-04-16 20:19:57 +0530

    issue successfully added under project

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,17 @@
 class IssuesController < ApplicationController
+  def new
+    @project = Project.find(params[:project_id])
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @issue = @project.issues.create(issue_params)
+    redirect_to project_path(@project)
+  end
+
+  private
+    def issue_params
+      params.require(:issue).permit(:Title, :Severity,:body)
+    end
+
 end
