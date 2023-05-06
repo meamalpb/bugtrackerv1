c069b9e91780f75718501648cec50267ba41a1f3
meamalpb <321amalpb.com@gmail.com>
2023-05-06 06:58:29 +0530

    migration files

---
+++ db/migrate/20230427030505_add_assignee_and_assigned_to_issues.rb
@@ -0,0 +1,6 @@
+class AddAssigneeAndAssignedToIssues < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :issues, :assignee, null: false, foreign_key: { to_table: :users }
+    add_reference :issues, :assigned, null: false, foreign_key: { to_table: :users }
+  end
+end
