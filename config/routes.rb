f629cc63a83112bb62abcc6e2bf39520c60a24a2
meamalpb <321amalpb.com@gmail.com>
2023-06-18 13:18:43 +0530

    styling done

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   root "home#index"
   get "/projectusers", to: "projects#project_user"
   get "/projectusers/:id" , to: "projects#project_user_show", as: "project_user_show"
+
   get "admin/projectusers/:id/add/:user_id" , to: "admin#add_user" , as: "project_add_user"
   get "admin/projectusers/:id/delete/:user_id" , to: "admin#delete_user" , as: "project_delete_user"
+
 
 end
