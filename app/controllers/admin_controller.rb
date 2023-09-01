bed62d7b93aa21a6b3f5d62c60204a6b8083ddf4
meamalpb <321amalpb.com@gmail.com>
2023-09-01 23:38:58 +0530

    Admin functions authenticated

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,33 @@
 
 class AdminController < ApplicationController
-def add_user
-  @project = Project.find(params[:id])
-  @user = User.find(params[:user_id])
-  @project.users << @user
-  redirect_to project_user_show_path(@project)
-end
+  before_action :set_pid_uid, only: [:add_user, :delete_user]
 
-def delete_user
-  @project = Project.find(params[:id])
-  @user = User.find(params[:user_id])
-  @project.users.delete(@user)
-  redirect_to project_user_show_path(@project)
-end
+  def add_user
+    if check_permissions
+      @project.users << @user
+    end
+    redirect_to project_user_show_path(@project)
+  end
+
+  def delete_user
+    if check_permissions
+      @project.users.delete(@user)
+    end
+    redirect_to project_user_show_path(@project)
+  end
+
+  private 
+  def set_pid_uid
+    @project = Project.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def check_permissions
+    if current_user.id == Project.find(params[:id]).project_lead_id || current_user.role=='admin'
+        return true 
+    else
+      return false
+    end
+  end
 
 end
